328286fa58ee3dfce4c7919715ac7622f368acf6
Yuta-31 <pineapple.gt@gmail.com>
2022-02-11 13:09:43 +0900

    heroku update

--- backend/index-heroku.js
+++ backend/index-heroku.js
@@ -3,6 +3,7 @@
 const os = require('os');
 const express = require('express');
 const app = express();
+const path = require('path');
 const server = require('http').createServer(app);
 const io = require('socket.io')(server, {
   cors: {
