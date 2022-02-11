e7ebd849b1bf5ace484cdbe2ca58585b455f903c
Yuta-31 <pineapple.gt@gmail.com>
2022-02-11 15:43:21 +0900

    mui update

--- frontend/src/VideoConnect5.js
+++ frontend/src/VideoConnect5.js
@@ -188,12 +188,12 @@ export default function VideoConnect4() {
       <Stack spacing={2}>
         <Grid container spcing={2}>
           <Grid item xs>
-            <Card sx={{ width: 400; height: 200; }}>
+            <Card sx={{ width: 400, height: 200 }}>
               <CardMedia component="video" playsInline autoPlay ref={localVideoRef} />
             </Card>
           </Grid>
           <Grid xs>
-            <Card sx={{ width: 400; height: 200; }}>
+            <Card sx={{ width: 400, height: 200 }}>
               <CardMedia component="video" playsInline autoPlay ref={remoteVideoRef} />
             </Card>
           </Grid>
